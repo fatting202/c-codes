6984f701cea8a12026966f0e3a1bc3c639e53904
ErPratham <44560611+pratham3164@users.noreply.github.com>
2019-10-14 01:30:24 +0530

    changed pattern
    display character and decleration changed

--- e5/fns-pattern.c
+++ e5/fns-pattern.c
@@ -1,23 +1,13 @@
 #include<stdio.h>
 #include<conio.h>
 int main()
-{
-	int n,i,j;
-	char a;
-	 printf("enetr the char ");
-     scanf("%c",&a);
-     printf("enetr the number ");
+{     printf("enetr the number ");
      scanf("%d",&n);
-    
-     for(i=0;i<n;i++ )
-     {  
-	   for(j=0;j<=i;j++ )
-	    {
-			 printf("%c",a);
-		 } 
-	      printf("\n"); 
+     for(int i=0;i<n;i++ )
+     {    for(int j=0;j<=i;j++ )
+	    printf("a",);
+	  printf("\n"); 
 	 }
-	
-	 getch();
+       getch();
 }
 
